821fbebd37697dc112dd678825e03fbc11dd32a9
rkusa <m@rkusa.st>
2015-02-19 10:50:16 +0100

    replace jquery-observe with selector-observer to ...
    - improve IE compatibility
    - fire events only ones
    - don’t fire remove events for moved items

--- touch-dnd.js
+++ touch-dnd.js
@@ -207,51 +207,164 @@
 
   var dragging = $.dragging = parent.$.dragging || new Dragging()
 
-  // from https://github.com/rkusa/jquery-observe
+  // from https://github.com/rkusa/selector-observer
   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
-  var Observer = function(target, selector, onAdded, onRemoved) {
-    var self    = this
-    this.target = target
+  function matches(el, selector) {
+    var fn = el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector
+    return fn ? fn.call(el, selector) : false
+  }
+  function toArr(nodeList) {
+    return Array.prototype.slice.call(nodeList)
+  }
 
+  // polyfill for IE < 11
+  var isOldIE = false
+  if (typeof MutationObserver === 'undefined') {
+    MutationObserver = function(callback) {
+      this.targets = []
+      this.onAdded = function(e) {
+        callback([{ addedNodes: [e.target], removedNodes: [] }])
+      }
+      this.onRemoved = function(e) {
+        callback([{ addedNodes: [], removedNodes: [e.target] }])
+      }
+    }
+
+    MutationObserver.prototype.observe = function(target) {
+      target.addEventListener('DOMNodeInserted', this.onAdded)
+      target.addEventListener('DOMNodeRemoved', this.onRemoved)
+      this.targets.push(target)
+    }
+
+    MutationObserver.prototype.disconnect = function() {
+      var target
+      while (target = this.targets.shift()) {
+        target.removeEventListener('DOMNodeInserted', this.onAdded)
+        target.removeEventListener('DOMNodeRemoved', this.onRemoved)
+      }
+    }
+
+    isOldIE = !!~navigator.appName.indexOf('Internet Explorer')
+  }
+
+  var SelectorObserver = function(targets, selector, onAdded, onRemoved) {
+    var self     = this
+    this.targets = targets instanceof NodeList
+                     ? Array.prototype.slice.call(targets)
+                     : [targets]
+
+    // support selectors starting with the childs only selector `>`
     var childsOnly = selector[0] === '>'
-      , search = childsOnly ? selector.substr(1) : selector
+    var search = childsOnly ? selector.substr(1) : selector
+    var initialized = false
 
-    function apply(nodes, callback) {
-      Array.prototype.slice.call(nodes).forEach(function(node) {
-        if (childsOnly && self.target[0] !== $(node).parent()[0]) return
-        if ($(node).is(search)) callback.call(node)
-        if (childsOnly) return
-        $(selector, node).each(function() {
-          callback.call(this)
+    function query(nodes, deep) {
+      var result = []
+
+      toArr(nodes).forEach(function(node) {
+        //ignore non-element nodes
+        if (node.nodeType !== 1) return;
+
+        // if looking for childs only, the node's parentNode
+        // should be one of our targets
+        if (childsOnly && self.targets.indexOf(node.parentNode) === -1) {
+          return
+        }
+
+        // test if the node itself matches the selector
+        if (matches(node, search)) {
+          result.push(node)
+        }
+
+        if (childsOnly || !deep) {
+          return
+        }
+
+        toArr(node.querySelectorAll(selector)).forEach(function(node) {
+          result.push(node)
         })
+      })
+
+      return result
+    }
+
+    function apply(nodes, deep, callback) {
+      if (!callback) {
+        return
+      }
+
+      // flatten
+      query(nodes, deep)
+      // filter unique nodes
+      .filter(function(node, i, self) {
+        return self.indexOf(node) === i
+      })
+      // execute callback
+      .forEach(function(node) {
+        callback.call(node)
       })
     }
 
-    this.observer = new MutationObserver(function(mutations) {
+    var timeout      = null
+    var addedNodes   = []
+    var removedNodes = []
+
+    function handle() {
       self.disconnect()
 
-      mutations.forEach(function(mutation) {
-        if (onAdded)   apply(mutation.addedNodes,   onAdded)
-        if (onRemoved) apply(mutation.removedNodes, onRemoved)
-      })
+      // filter moved elements (removed and re-added)
+      for (var i = 0, len = removedNodes.length; i < len; ++i) {
+        var index = addedNodes.indexOf(removedNodes[i])
+        if (index > -1) {
+          addedNodes.splice(index, 1)
+          removedNodes.splice(i--, 1)
+        }
+      }
+
+      //                ↓ IE workarounds ...
+      apply(addedNodes, !(initialized && isOldIE), onAdded)
+      apply(removedNodes, true, onRemoved)
+
+      addedNodes.length   = 0
+      removedNodes.length = 0
 
       self.observe()
+    }
+
+    this.observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        addedNodes.push.apply(addedNodes, mutation.addedNodes)
+        removedNodes.push.apply(removedNodes, mutation.removedNodes)
+      })
+
+      // IE < 10 fix: wait a cycle to gather all mutations
+      if (timeout) {
+        clearTimeout(timeout)
+      }
+      timeout = setTimeout(handle)
     })
 
     // call onAdded for existing elements
-    $(selector, target).each(function() {
-      onAdded.call(this)
-    })
+    if (onAdded) {
+      this.targets.forEach(function(target) {
+        apply(target.children, true, onAdded)
+      })
+    }
+
+    initialized = true
 
     this.observe()
   }
 
-  Observer.prototype.disconnect = function() {
+  SelectorObserver.prototype.disconnect = function() {
     this.observer.disconnect()
   }
 
-  Observer.prototype.observe = function() {
-    this.observer.observe(this.target[0], { childList: true, subtree: true })
+  SelectorObserver.prototype.observe = function() {
+    var self = this
+    this.targets.forEach(function(target) {
+      self.observer.observe(target, { childList: true, subtree: true })
+    })
   }
 
   var Draggable = function(element, opts) {
@@ -511,9 +624,11 @@
       self.el.trigger('sortable:create', self)
     })
 
-    this.observer = new Observer(this.el, this.opts.items, function() {
+    this.observer = new SelectorObserver(this.el[0], this.opts.items, function() {
     }, function() {
-      if (this === self.placeholder[0] || (dragging.el && this === dragging.el[0])) return
+      if (this === self.placeholder[0] || (dragging.el && this === dragging.el[0])) {
+        return
+      }
       var item = $(this)
       self.el.trigger('sortable:change', { item: item })
       self.el.trigger('sortable:update', { item: item, index: -1 })
